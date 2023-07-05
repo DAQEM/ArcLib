8f8455e29202a881cc71a8130b6d71a6ed3d3941
DAQEM <daqemyt@gmail.com>
2023-07-05 10:28:05 +0200

    Made ActionDataType register function publicly accessible.

--- common/src/main/java/com/daqem/arc/api/action/data/type/ActionDataType.java
+++ common/src/main/java/com/daqem/arc/api/action/data/type/ActionDataType.java
@@ -15,22 +15,22 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public interface ActionDataType<T> extends IActionDataType<T> {
 
-    IActionDataType<BlockState> BLOCK_STATE = create(Arc.getId("block_state"));
-    IActionDataType<BlockPos> BLOCK_POSITION = create(Arc.getId("block_position"));
-    IActionDataType<Integer> EXP_DROP = create(Arc.getId("exp_drop"));
-    IActionDataType<Integer> EXP_LEVEL = create(Arc.getId("exp_level"));
-    IActionDataType<Level> WORLD = create(Arc.getId("world"));
-    IActionDataType<DamageSource> DAMAGE_SOURCE = create(Arc.getId("damage_source"));
-    IActionDataType<Entity> ENTITY = create(Arc.getId("entity"));
-    IActionDataType<Float> DAMAGE_AMOUNT = create(Arc.getId("damage_amount"));
-    IActionDataType<Integer> DISTANCE_IN_CM = create(Arc.getId("swimming_distance_in_cm"));
-    IActionDataType<ItemStack> ITEM_STACK = create(Arc.getId("item_stack"));
-    IActionDataType<Item> ITEM = create(Arc.getId("item"));
-    IActionDataType<Advancement> ADVANCEMENT = create(Arc.getId("advancement"));
-    IActionDataType<MobEffectInstance> MOB_EFFECT_INSTANCE = create(Arc.getId("mob_effect_instance"));
-    IActionDataType<Recipe<?>> RECIPE = create(Arc.getId("recipe"));
+    IActionDataType<BlockState> BLOCK_STATE = register(Arc.getId("block_state"));
+    IActionDataType<BlockPos> BLOCK_POSITION = register(Arc.getId("block_position"));
+    IActionDataType<Integer> EXP_DROP = register(Arc.getId("exp_drop"));
+    IActionDataType<Integer> EXP_LEVEL = register(Arc.getId("exp_level"));
+    IActionDataType<Level> WORLD = register(Arc.getId("world"));
+    IActionDataType<DamageSource> DAMAGE_SOURCE = register(Arc.getId("damage_source"));
+    IActionDataType<Entity> ENTITY = register(Arc.getId("entity"));
+    IActionDataType<Float> DAMAGE_AMOUNT = register(Arc.getId("damage_amount"));
+    IActionDataType<Integer> DISTANCE_IN_CM = register(Arc.getId("swimming_distance_in_cm"));
+    IActionDataType<ItemStack> ITEM_STACK = register(Arc.getId("item_stack"));
+    IActionDataType<Item> ITEM = register(Arc.getId("item"));
+    IActionDataType<Advancement> ADVANCEMENT = register(Arc.getId("advancement"));
+    IActionDataType<MobEffectInstance> MOB_EFFECT_INSTANCE = register(Arc.getId("mob_effect_instance"));
+    IActionDataType<Recipe<?>> RECIPE = register(Arc.getId("recipe"));
 
-    private static <T> IActionDataType<T> create(ResourceLocation location) {
+    static <T> IActionDataType<T> register(ResourceLocation location) {
         return () -> location;
     }
 }
