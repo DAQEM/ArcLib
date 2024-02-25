dc96354a0ce512141729004610a7e1b20d4d7779
DAQEM <daqemyt@gmail.com>
2024-02-25 10:21:44 +0100

    Fixed On Strip Log Action

--- common/src/main/java/com/daqem/arc/mixin/MixinAxeItem.java
+++ common/src/main/java/com/daqem/arc/mixin/MixinAxeItem.java
@@ -2,6 +2,7 @@ package com.daqem.arc.mixin;
 
 import com.daqem.arc.event.triggers.PlayerEvents;
 import com.daqem.arc.api.player.ArcServerPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.AxeItem;
@@ -9,13 +10,21 @@ import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
+
 @Mixin(AxeItem.class)
 public abstract class MixinAxeItem extends DiggerItem {
+
+    @Shadow protected abstract Optional<BlockState> getStripped(BlockState arg);
 
     public MixinAxeItem(float f, float g, Tier tier, TagKey<Block> tagKey, Properties properties) {
         super(f, g, tier, tagKey, properties);
@@ -24,7 +33,12 @@ public abstract class MixinAxeItem extends DiggerItem {
     @Inject(at = @At("HEAD"), method = "useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;")
     public void useOn(UseOnContext useOnContext, CallbackInfoReturnable<InteractionResult> cir) {
         if (useOnContext.getPlayer() instanceof ArcServerPlayer serverPlayer) {
-            PlayerEvents.onStripLog(serverPlayer, useOnContext.getClickedPos(), useOnContext.getLevel());
+            BlockPos clickedPos = useOnContext.getClickedPos();
+            BlockState blockState = serverPlayer.arc$getLevel().getBlockState(clickedPos);
+            if (this.getStripped(blockState).isPresent()) {
+                PlayerEvents.onStripLog(serverPlayer, clickedPos, useOnContext.getLevel());
+
+            }
         }
     }
 }
