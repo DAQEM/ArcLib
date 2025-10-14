b1bd9d2b7aac87fb112cb6c0120b1df2ee438c48
DAQEM <daqemyt@gmail.com>
2025-10-14 10:29:28 +0200

    feat: add inverted condition descriptions

--- common/src/main/java/com/daqem/arc/api/condition/AbstractCondition.java
+++ common/src/main/java/com/daqem/arc/api/condition/AbstractCondition.java
@@ -24,11 +24,17 @@ public abstract class AbstractCondition implements ICondition {
 
     @Override
     public Component getName() {
+        if (inverted) {
+            return Arc.translatable("condition." + this.getType().getLocation().getPath() + ".inverted");
+        }
         return Arc.translatable("condition." + this.getType().getLocation().getPath());
     }
 
     @Override
     public Component getDescription(Object... args) {
+        if (inverted) {
+            return Arc.translatable("condition.description." + this.getType().getLocation().getPath() + ".inverted", args);
+        }
         return Arc.translatable("condition.description." + this.getType().getLocation().getPath(), args);
     }
 
