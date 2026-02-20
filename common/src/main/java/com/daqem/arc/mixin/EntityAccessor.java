c03a3e7c3ccd2f9d51bdb06976dee5b1abc8759a
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-02-20 20:33:12 +0100

    fix: isInRain method crash

---
+++ common/src/main/java/com/daqem/arc/mixin/EntityAccessor.java
@@ -0,0 +1,11 @@
+package com.daqem.arc.mixin;
+
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Entity.class)
+public interface EntityAccessor {
+    @Invoker("isInRain")
+    boolean arc$isInRain();
+}
